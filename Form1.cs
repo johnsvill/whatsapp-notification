c669214e42fa560ae68dee828c6c018c591c5de1
Jonathan Villeda <johnsvill@hotmail.com>
2019-12-26 20:18:45 -0600

    Subiendo proyecto

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SendWhatsAppNotification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string YourId = "QFMVIotzV060bVyF4qe3nWpvaG5zdmlsbDg2X2F0X2dtYWlsX2RvdF9jb20=";
+            string PhoneNumbers = "+50247310741";
+            string MensajeOrwal = textBox1.Text;
+
+            try
+            {
+                string Url = "https://NiceApi.net/API";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Headers.Add("X-APIId", YourId);
+                request.Headers.Add("X-APIMobile", PhoneNumbers);
+                using (StreamWriter streamOut = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamOut.Write(MensajeOrwal);
+                }
+                using(StreamReader streamIn = new StreamReader(request.GetResponse().GetResponseStream()))
+                {
+                    Console.WriteLine(streamIn.ReadToEnd());
+                }
+            }
+            catch(SystemException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
